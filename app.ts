07a247e6ad04fe2d3140d32bac2bf4334e182b2a
Gulyapas Poonkawinsiri <joetlobb@gmail.com>
2025-12-03 23:27:13 -0800

    Routing requests

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
 import * as http from 'http'; // TS
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
     // process.exit()
+
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    };
+    
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
