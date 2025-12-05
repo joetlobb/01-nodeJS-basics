a9bf12355a331b43a7ddcc7d5efc88ff470ef431
Gulyapas Poonkawinsiri <joetlobb@gmail.com>
2025-12-04 21:27:37 -0800

    Using the node ES module style system

--- app.ts
+++ app.ts
@@ -1,46 +1,6 @@
 import * as http from 'http'; // TS
-import * as fs from 'fs';
+import { requestHandler } from './routes.ts';
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    // process.exit()
-
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    };
-
-    if (url === '/message' && method === 'POST') {
-        const body: Buffer[] = [];
-        req.on('data', (chunk: Buffer) => {
-            body.push(chunk);
-            console.log(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            // fs.writeFileSync('message.txt', message); // will block next line execution until this write file is done
-            fs.writeFile('message.txt', message, (error) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            }); // this one write file asynchronously
-        });
-    };
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(requestHandler);
 
 server.listen(3000);
