75e17390fc8a14d0b3f2c2fc917152a54026627e
Gulyapas Poonkawinsiri <joetlobb@gmail.com>
2025-12-04 20:40:09 -0800

    Event driven code execution

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import * as http from 'http'; // TS
-import * as fs from 'fs'; 
+import * as fs from 'fs';
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers);
@@ -22,15 +22,15 @@ const server = http.createServer((req, res) => {
             body.push(chunk);
             console.log(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     };
 
     res.setHeader('Content-Type', 'text/html');
